1fb2db7bbf82861202f1362799a7a0dc7771349c
balasubramanian-s <balasubramanian.s@progress.com>
2025-03-26 19:44:58 +0530

    rename test folder
    Signed-off-by: balasubramanian-s <balasubramanian.s@progress.com>

--- tests/unit/file/linux_immutable_file_check_test.rb
+++
@@ -1,9 +0,0 @@
-require 'train-kubernetes'
-
-RSpec.describe 'TrainKubernetes' do
-  describe 'components' do
-    it 'should require the linux_immutable_file_check file' do
-      expect { require 'train-kubernetes/file/linux_immutable_file_check' }.not_to raise_error
-    end
-  end
-end
